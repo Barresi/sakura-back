5c7adeb4289b9dcae75aa41960e98aa8455a09c8
winter4 <mywinter4@gmail.com>
2023-08-19 17:57:41 +0300

    feat: add validation error messaging

--- source/middlewares.ts
+++ source/middlewares.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import pinoHttp from "pino-http";
 import Logger from "./clients/logger";
+import { ZodError } from "zod";
 
 const logger = Logger.instance;
 const expressLogger = pinoHttp({
@@ -20,6 +21,12 @@ export function preMiddlewares() {
 // - - - - - - //
 
 function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+  req.log.error(err);
+
+  if (err instanceof ZodError) {
+    res.status(404).json({ msg: "Некорректные данные" });
+  }
+
   res.status(500).json({ msg: err.message });
 }
 
