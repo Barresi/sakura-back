cf4ee5658ae7991498a52be1692be58f11fa9d7f
winter4 <mywinter4@gmail.com>
2023-08-19 17:57:19 +0300

    feat: add signup validation

--- source/api/v1/auth/auth.router.ts
+++ source/api/v1/auth/auth.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import ctrl from "./auth.controller";
+import valid from "./auth.validation";
 import wrap from "@src/api/async-wrapper";
 
 const auth = Router();
 
-auth.post("/signup", wrap(ctrl.signup));
+auth.post("/signup", valid.signup, wrap(ctrl.signup));
 
 export default auth;
---
+++ source/api/v1/auth/auth.validation.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+import { Request, Response, NextFunction } from "express";
+
+export default {
+  signup: function (req: Request, res: Response, next: NextFunction) {
+    // https://stackoverflow.com/questions/2370015/regular-expression-for-password-validation
+    const passwordRegex = /^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/;
+
+    const schema = z.object({
+      email: z.string().email().trim(),
+      password: z.string().regex(passwordRegex).trim(),
+    });
+
+    schema.parse(req.body);
+
+    next();
+  },
+};
