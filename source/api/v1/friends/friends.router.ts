c363d155f72a63ceb6ccbbe6ba0914254d2edd7a
Luffi <qSatan1s@yandex.ru>
2023-10-03 14:52:48 +0500

    use pre-push

--- source/api/v1/friends/friends.router.ts
+++ source/api/v1/friends/friends.router.ts
@@ -8,9 +8,7 @@ import { addFriend, removeFriend, acceptFriend } from './friends.controller';
 const friend = Router();
 
 friend.post('/add-friend', authJwt.verifyToken, addFriend);
-
 friend.delete('/remove-friend', authJwt.verifyToken, removeFriend);
-
 friend.post('/accept-friend', authJwt.verifyToken, acceptFriend);
 
 export default friend;
